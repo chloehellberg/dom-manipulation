3c9310f2a034308c772ceb4cfcb22412b59106a5
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-08-11 13:49:49 -0700

    Add children buttons, alert and this remove to goodbye and copy section
    Co-authored by: Amanda Otero <mandystar018@gmail.com>
    Co-authored by: David Cole <davidtcole@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,44 @@ $(document).ready(function() {
     $("ul#user").prepend("<li>Hello!</li>");
     $("ul#user").children("li").first().click(function() {
       alert('hi');
+      $(this).remove();
+      });
     });
     $("ul#webpage").prepend("<li>Why hello there!</li>");
     $("ul#webpage").children("li").first().click(function() {
       alert('hi');
+      $(this).remove();
     });
-  });
+
+
+    $("button#goodbye").click(function() {
+      $("ul#user").prepend("<li>Goodbye!</li>");
+      $("ul#user").children("li").first().click(function() {
+        alert('hi');
+        $(this).remove();
+      });
+    });
+    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+    $("ul#webpage").children("li").first().click(function() {
+      alert('hi');
+      $(this).remove();
+    });
 
   
-  $("button#goodbye").click(function() {
-    $("ul#user").prepend("<li>Goodbye!</li>");
-    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+    $("button#stop").click(function() {
+      $("ul#user").prepend("<li>Stop copying me!</li>");
+      $("ul#user").children("li").first().click(function() {
+        alert('hi');
+        $(this).remove();
+      });
+    });
+      $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+      $("ul#webpage").children("li").first().click(function() {
+        alert('hi');
+        $(this).remove();
+      });
   });
 
-  $("button#stop").click(function() {
-    $("ul#user").prepend("<li>Stop copying me!</li>");
-    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
-  });
-});
+
+
+
